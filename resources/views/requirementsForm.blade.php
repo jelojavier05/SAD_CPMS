62c0e4c6c0b77997b7fea3255c336cbaa12a9043
Jason Andrew Paredes <Jason Andrew Paredes>
2016-06-09 19:44:06 +0800

    requirements bc

--- resources/views/requirementsForm.blade.php
+++ resources/views/requirementsForm.blade.php
@@ -23,6 +23,63 @@ Guard Form
 		</nav>
 	</div>
 </div>
+<!----------------------------------------------PAGES-------------------------------------->
+<div class="row">
+	<div class="col s6 push-s4" style="margin-left:10px;">
+		<div class="container-fluid grey lighten-4 z-depth-1" style="border: 1px solid black; border-radius:5px;">
+			<legend><h4>Requirements</h4></legend></br>
+			<div class="row">
+				
+				<div class="col s6">
+					<div class="col s11 push-s1">	
+						<img class="materialboxed circle hoverable" width="150px;" height="150px;" src="{!! URL::asset('../Materialize/images/avatar5.png') !!}"/>
+					</div>
+					<div class="col s12">
+						<div class="file-field input-field">
+						  <div class="btn blue">
+							<span>File</span>
+							<input type="file">
+						  </div>
+						  <div class="file-path-wrapper col s5">
+							<input class="file-path validate" type="text">
+						  </div>
+						</div>
+					</div>
+				</div>
+				
+				
+				
+				<div class="col s6">
+					<input type="checkbox" id="1" />
+					<label for="1">TEST</label></br>
+					
+					<input type="checkbox" id="2" />
+      				<label for="2">TEST</label></br>
+					
+					<input type="checkbox" id="3" />
+      				<label for="3">TEST</label></br>
+					
+					<input type="checkbox" id="4" />
+      				<label for="4">TEST</label></br>
+	
+					<input type="checkbox" id="5" />
+      				<label for="5">TEST</label></br>
+				
+					<input type="checkbox" id="6" />
+      				<label for="6">TEST</label></br>
 
+					<input type="checkbox" id="7" />
+      				<label for="7">TEST</label></br>
 
+					<input type="checkbox" id="8" />
+      				<label for="8">TEST</label></br>
+				</div>
+				
+				
+				
+				
+			</div>
+		</div>
+	</div>
+</div>
 @stop
