53c24aa42de9cad1c3f2afc1f0656a380d75b51c
Jason Andrew Paredes <Jason Andrew Paredes>
2016-07-07 23:32:21 +0800

    hello

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -8,7 +8,7 @@ Guard Form
 
 <div class= "row">
 	<div class=" col s6 push-s6" style="margin-top: 100px;">
-		<a class="btn-large orange" href="/guardView">Guard List</a>
+		<a class="btn-large orange animated infinite tada" href="/guardView">Guard List</a>
 	</div>
 
 </div>
