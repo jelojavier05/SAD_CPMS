73d6cde8feb5e6d333467d257beb7d762b261e51
jelonicolejavier <joseph.javier17@yahoo.com>
2016-07-22 12:14:49 +0800

    gun view. MORE BUTTON.

--- app/Http/Controllers/GunViewController.php
+++ app/Http/Controllers/GunViewController.php
@@ -7,13 +7,34 @@ use App\Model\Leave;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Validator;
+use DB;
+use Input;
 
-
-class GunViewController extends Controller
-{
+class GunViewController extends Controller{
     
     public function index(){
-        return view('/gunView');
- 
+        return view('GunAdmin.gunView');
+    }
+    
+    public function getGuns(){
+        $guns = DB::table('tblgun')
+            ->join('tbltypeofgun', 'tbltypeofgun.intTypeOfGunID', '=' ,'tblgun.intTypeOfGunID')
+            ->select('tblgun.strGunName', 'tblgun.boolFlag', 'tblgun.intGunID', 'tbltypeofgun.strTypeOfGun')
+            ->where('tblgun.deleted_at', '=' ,null)
+            ->get();
+        
+        return response()->json($guns);
+    }
+    
+    public function getGun(){
+        $gunID = Input::get('gunID');
+        
+        $guns = DB::table('tblgun')
+            ->join('tblgunlicense', 'tblgunlicense.intGunID', '=', 'tblgun.intGunID')
+            ->select('tblgun.strMaker','tblgun.strSerialNumber','tblgunlicense.strLicenseNumber', 'tblgunlicense.dateIssued', 'tblgunlicense.dateExpiration')
+            ->where('tblgun.intGunID', '=' ,$gunID)
+            ->first();
+        
+        return response()->json($guns);
     }
 }
