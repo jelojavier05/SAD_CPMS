a83533926b8871b5c7b43327a543b5d4d16df916
JaniceArielle <janice.arielle08@gmail.com>
2016-08-08 00:47:11 +0000

    1

--- resources/views/securityguard/SecurityGuardDashboard.blade.php
+++ resources/views/securityguard/SecurityGuardDashboard.blade.php
@@ -174,7 +174,7 @@
                                     </div>
                                 
                                 
-<<<<<<< HEAD
+
                                 </div>
                              
 
@@ -194,7 +194,7 @@
                     <li><a  data-position="bottom" data-delay="50" data-tooltip="LOG OUT" id = 'btnLogout' class=" tooltipped"><i class="material-icons">input</i></a></li>
                
                 </ul>
-=======
+
                                 <li  id="notification_li">
                                     <a id="notificationLink" data-position="bottom" data-delay="50" data-tooltip="MESSAGES" href="/securityhomepage" class="tooltipped">
                                         <i class="mdi-content-inbox" style="font-size:2.1rem;color:white"></i>
@@ -205,8 +205,6 @@
                                 <li id="notification_li"><a  data-position="bottom" data-delay="50" data-tooltip="LOG OUT" id = 'btnLogout' class=" tooltipped"><i class="material-icons">input</i></a></li>
 							</ul>
         
-		
->>>>>>> e1a0ba640157b124340d4d12d4f4c82d42033c1d
         </div>
     
 </nav>
