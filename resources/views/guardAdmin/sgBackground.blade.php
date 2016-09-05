cbc6678ba3c13cf09633d95ac27861f20f243735
Jason Andrew Paredes <Jason Andrew Paredes>
2016-09-05 22:26:01 +0800

    test

--- resources/views/guardAdmin/sgBackground.blade.php
+++ resources/views/guardAdmin/sgBackground.blade.php
@@ -24,77 +24,94 @@ Guard Form
     </div>
 </div>
 
+
+
 <div class="row">
-    <div class="col s8 push-s3" style="margin-left:10px;">
-        <div class="container-fluid grey lighten-4 z-depth-1 ci animated slideInLeft" style="border: 1px solid black; border-radius:5px;">
-           <div class="row">
-					<div class="col l12 offset-l4">
-						
-						 <legend><h4>Armed Services</h4></legend>
-				
-					</div>
-			</div>
+	<div class="col s4 push-s3" style="margin-left:10px; margin-top:30px;">
+		 <div class="card animated slideInLeft z-depth-1" style="height:350px;">
+			 <div class="col l12">
+                    <div class="col l6 push-l2">
+                        <i class="material-icons" style="font-size:16rem">star</i>
+                    </div>
+                    <div class="col l6 pull-l1">
+                        <div class="row"></div>
+                        <div class="row"></div>
+                    </div>
+                </div>
+			 <div class="card-content">
+				 <span class="card-title activator grey-text text-darken-4">Armed Services<i class="material-icons right animated infinite flash" style="font-size:3rem">add</i></span>
+			 </div>
+			 <div class="card-reveal">
+				 <span class="card-title grey-text text-darken-4">Armed Services<i class="material-icons right">close</i></span>				 
 			
-            <div class="row">		
-				<div class="col s10 push-s1">
-                    <div class = "input-field col s4 offset-s8 pull-s8">    
-                        <select id = "armedService" >
-                            <option disabled selected value = "0">Choose armed services if any</option>
-                            @foreach($armedservices as $armedservice)
-                                <option id = "option{{$armedservice->intArmedServiceID}}" value = "{{$armedservice->intArmedServiceID}}">{{$armedservice->strArmedServiceName}}</option>
-                            @endforeach
-                        </select>
-						<label data-error="Incorrect" for="armedService">Armed Service</label>
-                    </div>
-                     <div class="input-field col s6">
-                        <select id = "armedServiceRank">
-                            <option value="" disabled selected>Choose Rank</option>  
-                        </select>
-                        <label data-error="Incorrect" for="armedServiceRank">Rank</label>
-                    </div>
-					<div class="input-field col s6">
-                        <select id = "armedServiceYear">
-                            <option value="" disabled selected>----</option>  
-                        </select>
-                        <label data-error="Incorrect" for="armedServiceYear">Year</label>
-                    </div>
+				 <div class="row">		
+					 <div class="col s12">
+						 <div class = "input-field col s8 offset-s4 pull-s4">    
+							 <select id = "armedService" >
+								 <option disabled selected value = "0">Choose armed services if any</option>
+								 @foreach($armedservices as $armedservice)
+								 <option id = "option{{$armedservice->intArmedServiceID}}" value = "{{$armedservice->intArmedServiceID}}">{{$armedservice->strArmedServiceName}}</option>
+								 @endforeach
+							 </select>
+							 <label data-error="Incorrect" for="armedService">Armed Service</label>
+						 </div>
+						 <div class="input-field col s6">
+							 <select id = "armedServiceRank">
+								 <option value="" disabled selected>Choose Rank</option>  
+							 </select>
+							 <label data-error="Incorrect" for="armedServiceRank">Rank</label>
+						 </div>
+						 <div class="input-field col s6">
+							 <select id = "armedServiceYear">
+								 <option value="" disabled selected>----</option>  
+							 </select>
+							 <label data-error="Incorrect" for="armedServiceYear">Year</label>
+						 </div>
 						
                     
-                    <div class="input-field col s6">
-                        <input class="with-gap" name="radio" type="radio" id="dischargedHonorably" value = "Honorably"/>
-                        <label for="dischargedHonorably">Discharged Honorably</label>
-                        <input class="with-gap" name="radio" type="radio" id="dischargedDishonorably"  value="Dishonorably"/>
-                        <label for="dischargedDishonorably">Discharged Dishonorably</label>
+						 <div class="input-field col s6">
+							 <input class="with-gap" name="radio" type="radio" id="dischargedHonorably" value = "Honorably"/>
+							 <label for="dischargedHonorably">Discharged Honorably</label>
+							 <input class="with-gap" name="radio" type="radio" id="dischargedDishonorably"  value="Dishonorably"/>
+							 <label for="dischargedDishonorably">Discharged Dishonorably</label>
+						 </div>
+						 <div class="input-field col s6">
+							 <input placeholder = " " id="reason" type="text" class="validate" pattern="[A-za-z0-9 ]{2,}" required="" aria-required="true" >
+							 <label data-error="Incorrect" for="reason">Reason</label>
+						 </div>
+					 </div>
+				 </div>
+			 </div>
+		</div>
+	</div>
+	
+	
+	
+	<div class="col s4 push-s3" style="margin-left:10px; margin-top:30px;">
+		 <div class="card animated slideInLeft z-depth-1" style="height:350px;">
+			 <div class="col l12">
+                    <div class="col l6 push-l2">
+                        <i class="material-icons" style="font-size:16rem">description</i>
                     </div>
-                    <div class="input-field col s6">
-                        <input placeholder = " " id="reason" type="text" class="validate" pattern="[A-za-z0-9 ]{2,}" required="" aria-required="true" >
-                        <label data-error="Incorrect" for="reason">Reason</label>
+                    <div class="col l6 pull-l1">
+                        <div class="row"></div>
+                        <div class="row"></div>
                     </div>
-				</div>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class ="row">
-    <div class = "col s8 push-s3" style="margin-left:10px;">
-        <div class="container-fluid grey lighten-4 z-depth-1 ci animated slideInLeft" style="border: 1px solid black; border-radius:5px;">
-              <div class="row">
-					<div class="col l12 offset-l4">
-						
-						 <legend><h4>Government Exam</h4></legend>
-				
-					</div>
-			</div>
-			
-			<button style="margin-top:-10%; margin-left:700px;" class="z-depth-1 btn green modal-trigger" href="#modalgovexamAdd">
-            <i class="material-icons left">add</i> ADD
-            </button>
-            <table class="striped white" id = "dataTable">
+                </div>
+			 <div class="card-content">
+				 <span class="card-title activator grey-text text-darken-4">Government Examinations<i class="material-icons right animated infinite flash" style="font-size:3rem">add</i></span>
+			 </div>
+			 <div class="card-reveal">
+				 <span class="card-title grey-text text-darken-4">Government Examinations<i class="material-icons right">close</i></span>
+				 
+            <div class='row'>
+				<div class="col s12">
+					<button style="margin-left:250px;" class="z-depth-1 btn green modal-trigger" href="#modalgovexamAdd">ADD</button>
+					<table class="striped white" id = "dataTable">
                 <thead>
                     <tr>
-						<th style="width:50px;"></th>
-                        <th style="width:50px;"></th>
+						<th ></th>
+                        <th ></th>
                         <th>Name</th>
                         <th>Ratings</th>
                         <th>Date Taken</th>
@@ -104,10 +121,19 @@ Guard Form
 
                 </tbody>
             </table>
-        </div>
-        <button style="margin-top:20px;" class=" z-depth-2 btn-large blue left animated slideInLeft" id="backArmed">Back</button>
-        <button style="margin-top:20px;" class=" z-depth-2 btn-large blue right animated slideInLeft" id = "nextArmed">Next</button>
-    </div>
+				</div>
+			</div>
+			 </div>
+		</div>
+	</div>
+	
+</div>
+
+<div class="row">
+	<div class="col s6 push-s4">
+		<button style="margin-top:1px;" class=" z-depth-2 btn-large blue left animated slideInLeft" id="backArmed">Back</button>
+        <button style="margin-top:1px;" class=" z-depth-2 btn-large blue right animated slideInLeft" id = "nextArmed">Next</button>
+	</div>
 </div>
 
 <!------------------------------------Modal govexamAdd--------------------------->
@@ -329,8 +355,8 @@ Guard Form
                 
                 var governmentExam = $('#addGovernmentExam').val();
                 table.row.add([
-                    '<button class="buttonUpdate btn" id = "id'+ governmentExamID +'" value = "'+ governmentExamID + '"><i class="material-icons">edit</i></button><label for="edit"></label>',
-                    '<button class="buttonDelete btn red " value = "' +governmentExamID +'"><i class="material-icons">delete</i></button>',
+                    '<button class="buttonUpdate btn col s12" id = "id'+ governmentExamID +'" value = "'+ governmentExamID + '"><i class="material-icons">edit</i></button><label for="edit"></label>',
+                    '<button class="buttonDelete btn red col s12" value = "' +governmentExamID +'"><i class="material-icons">delete</i></button>',
                     '<h id = "name' + governmentExamID+ '">' + governmentExam + '</h>',
                     '<h id = "rating' + governmentExamID + '">' + $('#strRating').val() + '</h>',
                     '<h id = "date' + governmentExamID + '">' +  $('#addDateTaken').val() + '</h>'
--- resources/views/testblade.blade.php
+++ resources/views/testblade.blade.php
@@ -7,77 +7,92 @@ Test
 @section('content')
 
 <div class="row">
-	<div class='col s4 push-s3'>
-		<ul class="collection with-header" id="" style="border:1px solid black;">
-			<li class="collection-header blue white-text " ><h5 style="font-weight:bold;">Log</h5>
-			</li>
-			<div class="sidenavhover" style="min-height:300px; max-height:300px;">
-				<li class="collection-item">
-					<div class="row">
-						<div class="col s5">	
-							Kobe Bean Bryant<!--<div style="font-weight:normal;" id = 'firstName'>&nbsp;&nbsp;&nbsp;Time-In</div>-->
-						</div>
-						<div class="col s3">
-							Time In
-						</div>
-						<div class="col s3">
-							12:30AM
-						</div>
-					</div>
-				</li>
-					
-				<li class="collection-item">
-					<div class="row">
-						<div class="col s5">	
-							Lebron James
-						</div>
-						<div class="col s3">
-							Time In
-						</div>
-						<div class="col s3">
-							04:30AM
-						</div>
-					</div>
-				</li>
+    <div class="col s8 push-s3" style="margin-left:10px;">
+        <div class="container-fluid grey lighten-4 z-depth-1 ci animated slideInLeft" style="border: 1px solid black; border-radius:5px;">
+           <div class="row">
+					<div class="col l12 offset-l4">
+						
+						 <legend><h4>Armed Services</h4></legend>
 				
-				<li class="collection-item">
-					<div class="row">
-						<div class="col s5">	
-							Lebron James
-						</div>
-						<div class="col s3">
-							Time Out
-						</div>
-						<div class="col s3">
-							12:30PM
-						</div>
 					</div>
-				</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
-				<li class="collection-item">test</li>
 			</div>
-		</ul>
-	</div>
-</div>
-<div class='row'>
-	<div class="col s6 push-s6">
-		<label for="birthdate" class='active'>Date of Birth</label>
-		<input type="date" class="datepicker" id="birthdate">
-	</div>
-
+			
+            <div class="row">		
+				<div class="col s10 push-s1">
+                    <div class = "input-field col s4 offset-s8 pull-s8">    
+                        <select id = "armedService" >
+                            <option disabled selected value = "0">Choose armed services if any</option>
+                            @foreach($armedservices as $armedservice)
+                                <option id = "option{{$armedservice->intArmedServiceID}}" value = "{{$armedservice->intArmedServiceID}}">{{$armedservice->strArmedServiceName}}</option>
+                            @endforeach
+                        </select>
+						<label data-error="Incorrect" for="armedService">Armed Service</label>
+                    </div>
+                     <div class="input-field col s6">
+                        <select id = "armedServiceRank">
+                            <option value="" disabled selected>Choose Rank</option>  
+                        </select>
+                        <label data-error="Incorrect" for="armedServiceRank">Rank</label>
+                    </div>
+					<div class="input-field col s6">
+                        <select id = "armedServiceYear">
+                            <option value="" disabled selected>----</option>  
+                        </select>
+                        <label data-error="Incorrect" for="armedServiceYear">Year</label>
+                    </div>
+						
+                    
+                    <div class="input-field col s6">
+                        <input class="with-gap" name="radio" type="radio" id="dischargedHonorably" value = "Honorably"/>
+                        <label for="dischargedHonorably">Discharged Honorably</label>
+                        <input class="with-gap" name="radio" type="radio" id="dischargedDishonorably"  value="Dishonorably"/>
+                        <label for="dischargedDishonorably">Discharged Dishonorably</label>
+                    </div>
+                    <div class="input-field col s6">
+                        <input placeholder = " " id="reason" type="text" class="validate" pattern="[A-za-z0-9 ]{2,}" required="" aria-required="true" >
+                        <label data-error="Incorrect" for="reason">Reason</label>
+                    </div>
+				</div>
+            </div>
+        </div>
+    </div>
 </div>
  
-	
+<!--gov exam	-->
+
+<div class ="row">
+    <div class = "col s8 push-s3" style="margin-left:10px;">
+        <div class="container-fluid grey lighten-4 z-depth-1 ci animated slideInLeft" style="border: 1px solid black; border-radius:5px;">
+              <div class="row">
+					<div class="col l12 offset-l4">
+						
+						 <legend><h4>Government Exam</h4></legend>
+				
+					</div>
+			</div>
+			
+			<button style="margin-top:-10%; margin-left:700px;" class="z-depth-1 btn green modal-trigger" href="#modalgovexamAdd">
+            <i class="material-icons left">add</i> ADD
+            </button>
+            <table class="striped white" id = "dataTable">
+                <thead>
+                    <tr>
+						<th style="width:50px;"></th>
+                        <th style="width:50px;"></th>
+                        <th>Name</th>
+                        <th>Ratings</th>
+                        <th>Date Taken</th>
+                    </tr>
+                </thead>
+                <tbody> 
+
+                </tbody>
+            </table>
+        </div>
+        <button style="margin-top:20px;" class=" z-depth-2 btn-large blue left animated slideInLeft" id="backArmed">Back</button>
+        <button style="margin-top:20px;" class=" z-depth-2 btn-large blue right animated slideInLeft" id = "nextArmed">Next</button>
+    </div>
+</div>
 	
 @stop
 
