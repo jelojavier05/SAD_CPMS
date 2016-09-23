59bac64984348dfc8d76c6a241b710847747e01b
jelonicolejavier <joseph.javier17@yahoo.com>
2016-09-23 16:33:15 +0800

    1

--- app/Http/Controllers/QueryGuardController.php
+++ app/Http/Controllers/QueryGuardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use DB;
 
 class QueryGuardController extends Controller
 {
@@ -19,69 +20,7 @@ class QueryGuardController extends Controller
         return view('/queries/guard');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    public function getQueryGuard(Request $request){
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
--- resources/views/queries/guard.blade.php
+++ resources/views/queries/guard.blade.php
@@ -13,22 +13,15 @@ Guards - Query
 </style>
 
 <div class="row" style="margin-top:-30px;">
-
-
-<div class="row"> 
-        
-    <div class="row">
- 
-     <div class="col s5 push-s3" style="margin-left:-2%">
-    
-                   <h3 class="blue-text animated fadeIn" style="font-family:Myriad Pro;margin-top:9.2%">Query - Guards</h3>
-                </div>
-    
-    </div>
-   
-    </div>
-    <div class="col s12 push-s1" style="margin-top:-4%;">
-        <div class="container blue-grey lighten-4 z-depth-2 animated fadeIn" style="padding-left:2%; padding-right:2%;">
+	<div class="row"> 
+		<div class="row">
+			<div class="col s5 push-s3" style="margin-left:-2%">
+				<h3 class="blue-text animated fadeIn" style="font-family:Myriad Pro;margin-top:9.2%">Query - Guards</h3>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 push-s1" style="margin-top:-4%;">
+		<div class="container blue-grey lighten-4 z-depth-2 animated fadeIn" style="padding-left:2%; padding-right:2%;">
 			<div class="row"></div>
 			<div class="row">
 				<div class="col s8">
@@ -41,7 +34,6 @@ Guards - Query
 						</select>
 						<label>Status</label>
 					</div>
-
 					<div class="input-field col s4">
 						<select id="selectGender">
 							<option selected value="">Choose Gender</option>
@@ -50,7 +42,6 @@ Guards - Query
 						</select>
 						<label>Gender</label>
 					</div>
-
 					<div class="input-field col s4">
 						<select disabled>
 							<option disabled selected>Choose Client</option>
@@ -60,71 +51,70 @@ Guards - Query
 						<label>Client</label>
 					</div>
 				</div>
-			
 				<div class="col s4">
 					<div class="input-field col s12">
 						<nav style="height:55px;">
-							<div class="nav-wrapper blue-grey lighten-3">
-								<form>
-									<div class="input-field" style="">
-										<input id="mySearch" type="search" placeholder="Search" required>
-										<label for="search"><i class="material-icons">search</i></label>									
-									</div>
-								</form>
-							</div>
+						<div class="nav-wrapper blue-grey lighten-3">
+							<form>
+								<div class="input-field" style="">
+									<input id="mySearch" type="search" placeholder="Search" required>
+									<label for="search"><i class="material-icons">search</i></label>									
+								</div>
+							</form>
+						</div>
 						</nav>
 					</div>	
 				</div>
-				
 			</div>
-        
-            <div class="row">
-                <div class="col s12" style="">
-                    <table class="striped" style="border-radius:10px;" id="dataTable">						
-                        <thead>
-                            <tr>                                                                                                
-                                <th class="blue darken-1 white-text">Name</th>
+			<div class="row">
+				<div class="col s12" style="">
+					<table class="striped" style="border-radius:10px;" id="dataTable">						
+						<thead>
+							<tr>                                                                                                
+								<th class="blue darken-1 white-text">Name</th>
 								<th class="blue darken-1 white-text">License Number</th>								
 								<th class="blue darken-1 white-text">Gender</th>
 								<th class="blue darken-1 white-text">Status</th>
-                                
-                            </tr>
-                        </thead>
-
-                        <tbody>
-							<tr>
-								<td>Jason Paredes</td>
-								<td>2013-04019-MN-0</td>
-								<td>Male</td>
-								<td>Active</td>
-								
 							</tr>
-							
-							<tr>
-								<td>Kobe Bryant</td>
-								<td>123-123</td>
-								<td>Male</td>
-								<td>Reliever</td>
-							</tr>
-							
-							<tr>
-								<td>Ronda Rousey</td>
-								<td>909-090</td>
-								<td>Female</td>
-								<td>Pending/Waiting</td>
-							</tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-
+						</thead>
+						<tbody>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 @stop
 		
 @section('script')
 <script>
 $(document).ready(function(){
+	$.ajax({
+		type: "GET",
+		url: "{{action('')}}",
+		beforeSend: function (xhr) {
+			var token = $('meta[name="csrf_token"]').attr('content');
+			if (token) {
+				return xhr.setRequestHeader('X-CSRF-TOKEN', token);
+			}
+		},
+		data: {
+
+		},
+		success: function(data){
+
+		},
+		error: function(data){
+			var toastContent = $('<span>Error Database.</span>');
+			Materialize.toast(toastContent, 1500,'red', 'edit');
+		}
+	});//ajax
+});
+</script>
+
+<script>
+	$(document).ready(function(){
 		
 		$("#dataTable").DataTable({
              "columns": [           
@@ -133,26 +123,22 @@ $(document).ready(function(){
 			null,
 			null
             ] ,  
-//		    "pagingType": "full_numbers",
 			"pageLength":5,
-//			"lengthMenu": [5,10,15,20],
-//			"bFilter": false
 			"bLengthChange": false
 
 
 		});
 	
-	search = $('#dataTable').DataTable();
-		$("#mySearch").keyup(function(){
-			search.search($(this).val()).draw();
-		});
-	
-	 var oTable = $('#dataTable').dataTable();
-	 $('select#selectStatus').change( function() { oTable.fnFilter( $(this).val(),3 ); } );
-	
-	var oTable = $('#dataTable').dataTable();
-	 $('select#selectGender').change( function() { oTable.fnFilter( $(this).val(),2 ); } );
-});
-	
+		search = $('#dataTable').DataTable();
+			$("#mySearch").keyup(function(){
+				search.search($(this).val()).draw();
+			});
+		
+		 var oTable = $('#dataTable').dataTable();
+		 $('select#selectStatus').change( function() { oTable.fnFilter( $(this).val(),3 ); } );
+		
+		var oTable = $('#dataTable').dataTable();
+		 $('select#selectGender').change( function() { oTable.fnFilter( $(this).val(),2 ); } );
+	});
 </script>		
 @stop
