c6a9a11d3c23ed11b25387a3ccdf0c037895a25f
Jason Andrew Paredes <Jason Andrew Paredes>
2016-09-23 16:42:29 +0800

    1

--- resources/views/queries/guard.blade.php
+++ resources/views/queries/guard.blade.php
@@ -13,20 +13,51 @@ Guards - Query
 </style>
 
 <div class="row" style="margin-top:-30px;">
+	<div class="row">         
 	<div class="row"> 
-		<div class="row">
-			<div class="col s5 push-s3" style="margin-left:-2%">
-				<h3 class="blue-text animated fadeIn" style="font-family:Myriad Pro;margin-top:9.2%">Query - Guards</h3>
-			</div>
-		</div>
+		<div class="col s5 push-s3" style="margin-left:-2%">    
+			<h3 class="blue-text animated fadeIn" style="font-family:Myriad Pro;margin-top:9.2%">Query - Guards</h3>
+		</div>    
+	</div>
+   
 	</div>
 	<div class="col s12 push-s1" style="margin-top:-4%;">
 		<div class="container blue-grey lighten-4 z-depth-2 animated fadeIn" style="padding-left:2%; padding-right:2%;">
 			<div class="row"></div>
-			
+						
 			<div class="row">
-				<div class="col s4 offset-s8">
-					<div class="input-field col s12">
+				<div class="col s12">
+					<div class="input-field col s3">
+						<select id="selectStatus">
+							<option selected value="">All</option>
+							<option value="Active">Active</option>
+							<option value="Pending/Waiting">Pending/Waiting</option>
+							<option value="Reliever">Reliever</option>
+						</select>
+						<label>Status</label>
+					</div>
+
+					<div class="input-field col s3">
+						<select id="selectGender">
+							<option selected value="">All</option>
+							<option value="Male (M)">Male (M)</option>
+							<option value="Female (F)">Female (F)</option>
+						</select>
+						<label>Gender</label>
+					</div>
+
+					<div class="input-field col s3">
+						<select id="selectClient">
+							<option selected>All</option>
+							<option value="LandBank Almanza">LandBank Almanza</option>
+							<option value="ChinaBank Pilar">ChinaBank Pilar</option>
+							<option value="Pacific Sta. Mesa">Pacific Sta. Mesa</option>
+						</select>
+						<label>Client</label>
+					</div>
+					
+					
+					<div class="input-field col s3">
 						<nav style="height:55px;">
 							<div class="nav-wrapper blue-grey lighten-3">
 								<form>
@@ -38,101 +69,64 @@ Guards - Query
 							</div>
 						</nav>
 					</div>	
+				
 				</div>
+			
+				
+				
 			</div>
-			<div class="row">
-				<div class="col s8">
-					<div class="input-field col s4">
-						<select id="selectStatus">
-							<option selected value="">All</option>
-							<option value="Active">Active</option>
-							<option value="Pending/Waiting">Pending/Waiting</option>
-							<option value="Reliever">Reliever</option>
-						</select>
-						<label>Status</label>
-					</div>
-					<div class="input-field col s4">
-						<select id="selectGender">
-							<option selected value="">All</option>
-							<option value="Male (M)">Male (M)</option>
-							<option value="Female (F)">Female (F)</option>
-						</select>
-						<label>Gender</label>
-					</div>
-					<div class="input-field col s4">
-						<select id="selectClient">
-							<option selected>All</option>
-							<option value="LandBank Almanza">LandBank Almanza</option>
-							<option value="ChinaBank Pilar">ChinaBank Pilar</option>
-							<option value="Pacific Sta. Mesa">Pacific Sta. Mesa</option>
-						</select>
-						<label>Client</label>
-					</div>
-				</div>
-				<div class="col s4">
-					<div class="input-field col s12">
-						<nav style="height:55px;">
-						<div class="nav-wrapper blue-grey lighten-3">
-							<form>
-								<div class="input-field" style="">
-									<input id="mySearch" type="search" placeholder="Search" required>
-									<label for="search"><i class="material-icons">search</i></label>									
-								</div>
-							</form>
-						</div>
-						</nav>
-					</div>	
-				</div>
-			</div>
-			<div class="row">
-				<div class="col s12" style="">
-					<table class="striped" style="border-radius:10px;" id="dataTable">						
-						<thead>
-							<tr>                                                                                                
-								<th class="blue darken-1 white-text">Name</th>
-								<th class="blue darken-1 white-text">License Number</th>								
+        
+            <div class="row">
+                <div class="col s12" style="">
+                    <table class="striped" style="border-radius:10px;" id="tblqueryGuards">						
+                        <thead>
+                            <tr>                                                                                                
+                                <th class="blue darken-1 white-text">License Number</th>
+								<th class="blue darken-1 white-text">Name</th>															
 								<th class="blue darken-1 white-text">Gender</th>
 								<th class="blue darken-1 white-text">Status</th>
+								<th class="blue darken-1 white-text">Client</th>
+                                
+                            </tr>
+                        </thead>
+
+                        <tbody>
+							<tr>
+								<td>2013-04019-MN-0</td>
+								<td>Jason Paredes</td>								
+								<td>Male (M)</td>
+								<td>Active</td>
+								<td>ChinaBank Pilar</td>
+								
 							</tr>
-						</thead>
-						<tbody>
-						</tbody>
-					</table>
-				</div>
-			</div>
-		</div>
+							
+							<tr>
+								<td>123-123</td>
+								<td>Kobe Bryant</td>								
+								<td>Male (M)</td>
+								<td>Reliever</td>
+								<td>LandBank Almanza</td>
+							</tr>
+							
+							<tr>
+								<td>909-090</td>
+								<td>Ronda Rousey</td>								
+								<td>Female (F)</td>
+								<td>Pending/Waiting</td>
+								<td>Pacific Sta. Mesa</td>
+							</tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
 	</div>
-</div>
+
 @stop
 		
 @section('script')
 <script>
 $(document).ready(function(){
-	$.ajax({
-		type: "GET",
-		url: "{{action('')}}",
-		beforeSend: function (xhr) {
-			var token = $('meta[name="csrf_token"]').attr('content');
-			if (token) {
-				return xhr.setRequestHeader('X-CSRF-TOKEN', token);
-			}
-		},
-		data: {
-
-		},
-		success: function(data){
-
-		},
-		error: function(data){
-			var toastContent = $('<span>Error Database.</span>');
-			Materialize.toast(toastContent, 1500,'red', 'edit');
-		}
-	});//ajax
-});
-</script>
-
-<script>
-	$(document).ready(function(){
 		
 		$("#tblqueryGuards").DataTable({
              "columns": [           
@@ -142,22 +136,35 @@ $(document).ready(function(){
 			null,
 			null
             ] ,  
+//		    "pagingType": "full_numbers",
 			"pageLength":5,
+//			"lengthMenu": [5,10,15,20],
+//			"bFilter": false
 			"bLengthChange": false
 
 
 		});
 	
-		search = $('#dataTable').DataTable();
-			$("#mySearch").keyup(function(){
-				search.search($(this).val()).draw();
-			});
-		
-		 var oTable = $('#dataTable').dataTable();
-		 $('select#selectStatus').change( function() { oTable.fnFilter( $(this).val(),3 ); } );
-		
-		var oTable = $('#dataTable').dataTable();
-		 $('select#selectGender').change( function() { oTable.fnFilter( $(this).val(),2 ); } );
-	});
+	search = $('#tblqueryGuards').DataTable();
+		$("#mySearch").keyup(function(){
+			search.search($(this).val()).draw();
+		});
+	
+	 var oTable = $('#tblqueryGuards').dataTable();
+	 $('select#selectStatus').change( function() {
+		 oTable.fnFilter( $(this).val(),3 ); 
+	 });
+	
+	var oTable = $('#tblqueryGuards').dataTable();
+	 $('select#selectGender').change( function() {
+		 oTable.fnFilter( $(this).val(),2 );
+	 });
+	
+	var oTable = $('#tblqueryGuards').dataTable();
+	 $('select#selectClient').change( function() {
+		 oTable.fnFilter( $(this).val(),4);
+	 });
+});
+	
 </script>		
 @stop
